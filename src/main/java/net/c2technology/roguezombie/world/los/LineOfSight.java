a4093d46397466fe47c9237362b3b4ebe824c11c
Chris <Chris@Chris-PC>
2015-08-31 23:22:46 -0400

    Corrected Line of Sight to be of proper range Corrected remembered tiles to properly fade

--- src/main/java/net/c2technology/roguezombie/world/los/LineOfSight.java
+++ src/main/java/net/c2technology/roguezombie/world/los/LineOfSight.java
@@ -8,6 +8,7 @@ package net.c2technology.roguezombie.world.los;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
+import net.c2technology.roguezombie.world.Cardinal;
 import net.c2technology.roguezombie.world.Coordinate;
 
 /**
@@ -30,8 +31,10 @@ public class LineOfSight implements Iterable<Coordinate> {
     }
 
     /**
-     * Uses Bresenham's line algorithm to determine the tiles within the given
-     * line of sight.
+     * Uses a modified Bresenham's line algorithm to determine the tiles within
+     * the given line of sight. The modification is due to the origin point of
+     * the grid being the top-left most coordinate (0,0). All coordinate points
+     * are positive.
      *
      * @param origin The start of line of sight
      * @param destination The target for line of sight. This might not
@@ -39,30 +42,75 @@ public class LineOfSight implements Iterable<Coordinate> {
      * @return
      */
     public static LineOfSight getLineOfSight(Coordinate origin, Coordinate destination) {
-        List<Coordinate> coordinates = new ArrayList();
 
+        //y = ((y1-y0)/(x1-x0))(x-x0)+y0
+        List<Coordinate> coordinates = new ArrayList();
         int x0 = origin.getX();
         int y0 = origin.getY();
         int x1 = destination.getX();
         int y1 = destination.getY();
-
-        int deltaX = x1 - x0;
-        int deltaY = y1 - y0;
-
-        int delta = 2 * deltaY - deltaX;
+        Cardinal direction = Cardinal.getDirection(origin, destination);
         coordinates.add(new Coordinate(x0, y0));
-        int y = y0;
-        for (int x = x0 + 1; x <= x1; x++) {
-            if (delta > 0) {
-                y = y + 1;
-                coordinates.add(new Coordinate(x, y));
-                delta = delta + (2 * deltaY - 2 * deltaX);
-            } else {
-                coordinates.add(new Coordinate(x, y));
-                delta = delta + (2 * deltaY);
-            }
+
+        //Determine the line of sight based on orthagonal cardinal direction
+        switch (direction) {
+            case NORTH_EAST:
+            case SOUTH_EAST:
+            case EAST:
+                for (int x = x0 + 1; x <= x1; x++) {
+                    int y = getY(x0, x1, y0, y1, x);
+                    coordinates.add(new Coordinate(x, y));
+                }
+                break;
+            case NORTH_WEST:
+            case SOUTH_WEST:
+            case WEST:
+                //going left
+                for (int x = x0 - 1; x >= x1; x--) {
+                    int y = getY(x0, x1, y0, y1, x);
+                    coordinates.add(new Coordinate(x, y));
+                }
+                break;
+            case NORTH:
+                //going up
+                for (int y = y0 - 1; y >= y1; y--) {
+                    coordinates.add(new Coordinate(x0, y));
+                }
+                break;
+            case SOUTH:
+                //going down
+                for (int y = y0 + 1; y <= y1; y++) {
+                    coordinates.add(new Coordinate(x0, y));
+                }
+                break;
+            default:
+                //NONE or UNKNOWN
+                break;
         }
         return new LineOfSight(coordinates);
     }
 
+    /**
+     * Performs the Pythagorean Theorem to determine if the {@code target} is
+     * within the given {@code radius} of the {@code origin} hypotenuse (c) is
+     * greater than
+     *
+     * @param origin
+     * @param target
+     * @param radius
+     * @return
+     */
+    public static boolean insideRadius(Coordinate origin, Coordinate target, int radius) {
+
+        int a = origin.horizontalDelta(target);
+        int b = origin.verticalDelta(target);
+        int a2 = a * a;
+        int b2 = b * b;
+        int c2 = radius * radius;
+        return a2 + b2 <= c2;
+    }
+
+    private static int getY(int x0, int x1, int y0, int y1, int x) {
+        return ((y1 - y0) / (x1 - x0)) * (x - x0) + y0;
+    }
 }
